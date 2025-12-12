6a4c64effcd84858bd58943607587959f96cc356
palantir49 <pupkin.dus@yandex.ru>
2025-12-12 13:28:53 +0300

    [feature] Decalre generic repositories method

--- Homeworks/Base/src/PromoCodeFactory.Core/Abstractions/Repositories/IRepository.cs
+++ Homeworks/Base/src/PromoCodeFactory.Core/Abstractions/Repositories/IRepository.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using PromoCodeFactory.Core.Domain;
 
-namespace PromoCodeFactory.Core.Abstractions.Repositories
-{
-    public interface IRepository<T> where T: BaseEntity
-    {
-        Task<IEnumerable<T>> GetAllAsync();
+namespace PromoCodeFactory.Core.Abstractions.Repositories;
 
-        Task<T> GetByIdAsync(Guid id);
-    }
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<IEnumerable<T>> GetAllAsync();
+
+    Task<T> GetByIdAsync(Guid id);
+
+    Task AddAsync(T entity);
+
+    Task UpdateAsync(T entity);
+
+    Task DeleteAsync(Guid id);
+
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
 }
