64056436655f09c007b977ce6c46c811b57422c4
palantir49 <pupkin.dus@yandex.ru>
2025-12-12 13:28:14 +0300

    [feature] Add request model

---
+++ Homeworks/Base/src/PromoCodeFactory.WebHost/Models/EmployeeRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PromoCodeFactory.WebHost.Models;
+
+public record EmployeeRequest(
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Имя не может быть пустым")]
+    string FirstName,
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Фамилия не может быть пустой")]
+    string LastName,
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Email не может быть пустым")]
+    [EmailAddress(ErrorMessage = "Неверный формат Email")]
+    string Email,
+    [Required(ErrorMessage = "Роль обязательна")]
+    List<string> Roles,
+    [Range(0, int.MaxValue, ErrorMessage = "Количество промокодов должно быть неотрицательным")]
+    int AppliedPromocodesCount);
