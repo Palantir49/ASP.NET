c007f4f03c02542d64103106b6914d34e6a2aa99
palantir49 <pupkin.dus@yandex.ru>
2025-12-21 17:09:24 +0300

    [feature] Add migrations

---
+++ Homeworks/EF/src/PromoCodeFactory.DataAccess/Migrations/20251221135052_AddIndexToPromoCode.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PromoCodeFactory.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexToPromoCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_PromoCodes_Code",
+                table: "PromoCodes",
+                column: "Code");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PromoCodes_Code",
+                table: "PromoCodes");
+        }
+    }
+}
