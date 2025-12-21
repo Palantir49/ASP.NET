f243a6bb26b14fcd7c9606f03fdee3e039f34394
palantir49 <pupkin.dus@yandex.ru>
2025-12-21 17:09:53 +0300

    [feature] Configure dependencies for DAL

---
+++ Homeworks/EF/src/PromoCodeFactory.DataAccess/Extensions/ServiceExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PromoCodeFactory.DataAccess.Context;
+using PromoCodeFactory.DataAccess.Repositories.Implements;
+using PromoCodeFactory.DataAccess.Repositories.Interfaces;
+
+namespace PromoCodeFactory.DataAccess.Extensions;
+
+public static class ServiceExtension
+{
+    public static void ConfigureDataAccess(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Db");
+        services.AddDbContext<PromoCodeFactoryDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+                options.EnableSensitiveDataLogging();
+            }
+        );
+        services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+    }
+}
