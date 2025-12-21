480d70c85a47a1435ef80d7a5d99f23c434e3039
palantir49 <pupkin.dus@yandex.ru>
2025-12-21 17:11:07 +0300

    [feature] DbInitializer

---
+++ Homeworks/EF/src/PromoCodeFactory.DataAccess/Data/DbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PromoCodeFactory.DataAccess.Context;
+
+namespace PromoCodeFactory.DataAccess.Data;
+
+public static class DbInitializer
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<PromoCodeFactoryDbContext>();
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.MigrateAsync();
+        Seed(dbContext);
+    }
+
+    private static void Seed(PromoCodeFactoryDbContext dbContext)
+    {
+        if (!dbContext.Employees.Any()) dbContext.Employees.AddRange(FakeDataFactory.Employees);
+        if (!dbContext.PromoCodes.Any()) dbContext.Customers.AddRange(FakeDataFactory.Customers);
+        dbContext.SaveChanges();
+    }
+}
