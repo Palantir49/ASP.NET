7e531b563f6583d5aa00a90e499dffe9216bacb6
palantir49 <pupkin.dus@yandex.ru>
2025-12-28 13:24:41 +0300

    [feature] Add PartnerLimitService.cs and PartnerLimitOperationResult.cs to dedicate setting partner limit logic to separate service

---
+++ Homeworks/UnitTests/src/PromoCodeFactory.WebHost/Results/PartnerLimitOperationResult.cs
@@ -0,0 +1,28 @@
+﻿using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+
+namespace PromoCodeFactory.WebHost.Results;
+
+public class PartnerLimitOperationResult
+{
+    private PartnerLimitOperationResult(bool isSuccess, string errorMessage = null,
+        PartnerPromoCodeLimit partnerPromoCodeLimit = null)
+    {
+        IsSuccess = isSuccess;
+        ErrorMessage = errorMessage;
+        NewLimitId = partnerPromoCodeLimit;
+    }
+
+    public bool IsSuccess { get; }
+    public string ErrorMessage { get; }
+    public PartnerPromoCodeLimit NewLimitId { get; }
+
+    public static PartnerLimitOperationResult Success(PartnerPromoCodeLimit partnerPromoCodeLimit)
+    {
+        return new PartnerLimitOperationResult(true, partnerPromoCodeLimit: partnerPromoCodeLimit);
+    }
+
+    public static PartnerLimitOperationResult Failure(string errorMessage)
+    {
+        return new PartnerLimitOperationResult(false, errorMessage);
+    }
+}
---
+++ Homeworks/UnitTests/src/PromoCodeFactory.WebHost/Services/PartnerLimitService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.WebHost.Models;
+using PromoCodeFactory.WebHost.Results;
+
+namespace PromoCodeFactory.WebHost.Services;
+
+public class PartnerLimitService
+{
+    public PartnerLimitOperationResult ProcessLimitAsync(
+        Partner partner,
+        SetPartnerPromoCodeLimitRequest request,
+        DateTime currentTime)
+    {
+        // Валидация лимита
+        if (request.Limit <= 0)
+            return PartnerLimitOperationResult.Failure("Лимит должен быть больше 0");
+
+        var activeLimit = partner.PartnerLimits.FirstOrDefault(x => !x.CancelDate.HasValue);
+
+        if (activeLimit != null)
+        {
+            partner.NumberIssuedPromoCodes = 0;
+            activeLimit.CancelDate = currentTime;
+        }
+
+        var newLimit = new PartnerPromoCodeLimit
+        {
+            Limit = request.Limit,
+            Partner = partner,
+            PartnerId = partner.Id,
+            CreateDate = currentTime,
+            EndDate = request.EndDate
+        };
+
+        partner.PartnerLimits.Add(newLimit);
+
+        return PartnerLimitOperationResult.Success(newLimit);
+    }
+}
