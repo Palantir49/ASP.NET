8e3756f04fba79297cc7b130b8abdabf8fdf0100
palantir49 <pupkin.dus@yandex.ru>
2026-04-19 15:32:03 +0300

    [fix]

--- Homeworks/NoSQL/src/Pcf.ReceivingFromPartner/Pcf.ReceivingFromPartner.Integration/GivingPromoCodeToCustomerGateway.cs
+++ Homeworks/NoSQL/src/Pcf.ReceivingFromPartner/Pcf.ReceivingFromPartner.Integration/GivingPromoCodeToCustomerGateway.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Pcf.ReceivingFromPartner.Core.Abstractions.Gateways;
 using Pcf.ReceivingFromPartner.Core.Domain;
