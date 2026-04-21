67a82403434559cdb0bf764faabb23b996570aa4
palantir49 <pupkin.dus@yandex.ru>
2026-04-21 23:14:51 +0300

    [feature] Add project with public events for event bus

---
+++ Homeworks/RabbitMQ/src/Pcf.Events/PartnerManagerReceivingIntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿using Pcf.MessageBus.Abstractions;
+
+namespace Pcf.Events;
+
+public sealed record PartnerManagerReceivingIntegrationEvent : IntegrationEvent
+{
+    public Guid EmployeeId { get; set; }
+}
---
+++ Homeworks/RabbitMQ/src/Pcf.Events/PromoCodeReceivingIntegrationEvent.cs
@@ -0,0 +1,20 @@
+﻿using Pcf.MessageBus.Abstractions;
+
+namespace Pcf.Events;
+
+public sealed record PromoCodeReceivingIntegrationEvent : IntegrationEvent
+{
+    public string? ServiceInfo { get; set; }
+
+    public Guid PartnerId { get; set; }
+
+    public Guid PromoCodeId { get; set; }
+
+    public string? PromoCode { get; set; }
+
+    public Guid PreferenceId { get; set; }
+
+    public string? BeginDate { get; set; }
+
+    public string? EndDate { get; set; }
+}
