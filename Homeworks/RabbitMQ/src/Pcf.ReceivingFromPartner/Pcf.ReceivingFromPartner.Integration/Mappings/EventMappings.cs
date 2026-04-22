1e327a7d565685abb76e1c2f5686ca3ba08b1a9c
palantir49 <pupkin.dus@yandex.ru>
2026-04-22 16:20:16 +0300

    [feature] Add extension methods to mapping events and models

---
+++ Homeworks/RabbitMQ/src/Pcf.ReceivingFromPartner/Pcf.ReceivingFromPartner.Integration/Mappings/EventMappings.cs
@@ -0,0 +1,24 @@
+﻿using Pcf.Events;
+using Pcf.ReceivingFromPartner.Core.Domain;
+
+namespace Pcf.ReceivingFromPartner.Integration.Mappings;
+
+public static class EventMappings
+{
+    extension(PromoCode promoCode)
+    {
+        public PromoCodeReceivingIntegrationEvent ToPromoCodeReceivingIntegrationEvent()
+        {
+            return new PromoCodeReceivingIntegrationEvent
+            {
+                PartnerId = promoCode.Partner.Id,
+                BeginDate = promoCode.BeginDate.ToShortDateString(),
+                EndDate = promoCode.EndDate.ToShortDateString(),
+                PreferenceId = promoCode.PreferenceId,
+                PromoCode = promoCode.Code,
+                ServiceInfo = promoCode.ServiceInfo,
+                PartnerManagerId = promoCode.PartnerManagerId
+            };
+        }
+    }
+}
