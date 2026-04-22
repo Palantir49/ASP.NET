306627162278d7a7fdeb5e1d68a2e9b7a1db6c10
palantir49 <pupkin.dus@yandex.ru>
2026-04-22 18:17:58 +0300

    [feature] Add EmployeeService

---
+++ Homeworks/RabbitMQ/src/Pcf.Administration/Pcf.Administration.Core/Realizations/Services/EmployeeService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pcf.Administration.Core.Abstractions.Repositories;
+using Pcf.Administration.Core.Abstractions.Services;
+using Pcf.Administration.Core.Domain.Administration;
+
+namespace Pcf.Administration.Core.Realizations.Services;
+
+public sealed class EmployeeService(IRepository<Employee> employeeRepository) : IEmployeeService
+{
+    public async Task UpdateAppliedPromoCodesAsync(Guid id)
+    {
+        var employee = await employeeRepository.GetByIdAsync(id);
+
+        if (employee == null)
+            throw new KeyNotFoundException($"Сотрудника с таким {id} не найдено");
+
+        employee.AppliedPromocodesCount++;
+
+        await employeeRepository.UpdateAsync(employee);
+    }
+}
